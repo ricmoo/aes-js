16746cb5300e69526c46c0ee61ce7e26be0d25f9
Andrey Kutejko <andy128k@gmail.com>
2015-11-21 13:50:49 +0200

    extract CBC module

--- src/index.js
+++ src/index.js
@@ -3,54 +3,8 @@
 var AES = require('./aes');
 var copyIV = require('./iv');
 
-function xorBlocks(result, first, second) {
-  for (var i = 0; i < result.length; i++) {
-    result[i] = first[i] ^ second[i];
-  }
-}
-
 var ecb = require('./ecb');
-
-
-    /**
-     *  Mode Of Operation - Cipher Block Chaining (CBC)
-     */
-    var ModeOfOperationCBC = function(key, iv) {
-        if (iv && iv.length !== 16)
-            throw new Error('initialation vector iv must be of length 16');
-
-        this.description = "Cipher Block Chaining";
-        this.name = "cbc";
-        this._lastCipherblock = copyIV(iv);
-
-        this._aes = new AES(key);
-    }
-
-    ModeOfOperationCBC.prototype.encrypt = function(plaintext, result) {
-        if (plaintext.length !== 16) {
-            throw new Error('plaintext must be a block of size 16');
-        }
-        if (result.length !== 16) {
-            throw new Error('result must be a block of size 16');
-        }
-
-        xorBlocks(result, this._lastCipherblock, plaintext);
-        this._aes.encrypt(result, result);
-        this._lastCipherblock = result.slice();
-    }
-
-    ModeOfOperationCBC.prototype.decrypt = function(ciphertext, result) {
-        if (ciphertext.length !== 16) {
-            throw new Error('ciphertext must be a block of size 16');
-        }
-        if (result.length !== 16) {
-            throw new Error('result must be a block of size 16');
-        }
-
-        this._aes.decrypt(ciphertext, result);
-        xorBlocks(result, result, this._lastCipherblock);
-        this._lastCipherblock = ciphertext.slice();
-    }
+var cbc = require('./cbc');
 
 
     /**
@@ -219,7 +173,7 @@ var ecb = require('./ecb');
     // The bsic modes of operation as a map
     var ModeOfOperation = {
         ecb: ecb,
-        cbc: ModeOfOperationCBC,
+        cbc: cbc,
         cfb: ModeOfOperationCFB,
         ofb: ModeOfOperationOFB,
         ctr: ModeOfOperationCTR
